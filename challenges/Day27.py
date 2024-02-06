445de72961080c1995288b3bba5511e1dddc233b
Sonali Goel <112942761+goelsonali@users.noreply.github.com>
2024-02-06 23:34:04 +0000

    Day27 Tests updated

--- challenges/Day27.py
+++ challenges/Day27.py
@@ -3,18 +3,17 @@
 # Test Cases :
 # 1. Check if the string is empty.
 # 2. Check if the list is mixed case.
-# 3. Check if the list is lower case.
-# 4. Check if the list has only 1 string.
+# 4. Check if the list is lower case.
 
 def sort_list_alphabetically(input_list_of_strings):
     if input_list_of_strings:
-        lower_case_string = list(map(lambda s: s.lower(), input_list_of_strings))
-        lower_case_string.sort()
-        return lower_case_string
+        return sorted(input_list_of_strings, key= str.lower)
     else:
         return 'The list is empty'
 
 
 if __name__ == '__main__':
     in_list_of_string = ['d', 'abc', 'b', 'cde', 'e', 'a']
+    print(f' The input list of string is : {in_list_of_string} the sorted list alphabetically is {sort_list_alphabetically(in_list_of_string)}')
+    in_list_of_string = ['D', 'abc', 'Bcg', 'cde', 'F', 'a', 'b']
     print(f' The input list of string is : {in_list_of_string} the sorted list alphabetically is {sort_list_alphabetically(in_list_of_string)}')
---
+++ tests/test_Day27.py
@@ -0,0 +1,28 @@
+# Test class for the Day27 challenge
+# Create a program that sorts a list of strings alphabetically.
+
+from challenges.Day27 import sort_list_alphabetically
+
+# Test Cases :
+# 1. Check if the string is empty.
+# 2. Check if the list is mixed case.
+# 4. Check if the list is lower case.
+
+
+def test_check_list():
+    # TestCase 1:
+    expected_result = 'The list is empty'
+    in_list = []
+    assert expected_result == sort_list_alphabetically(in_list)
+
+    # TestCase 2:
+    expected_result = ['a', 'be', 'I', 'python', 'The']
+    in_list = ['a', 'I', 'The', 'python', 'be']
+    assert expected_result == sort_list_alphabetically(in_list)
+
+    # TestCase 3:
+    expected_result = ['i', 'learn', 'python', 'to', 'want']
+    in_list = ['i', 'want', 'to', 'learn', 'python']
+    assert expected_result == sort_list_alphabetically(in_list)
+
+
